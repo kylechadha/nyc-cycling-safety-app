5d393698395b4878db74a89f6cfd124024544d79
Kyle Chadha <kyle.chadha@buzzfeed.com>
2014-08-04 23:27:46 -0400

    Add injury and death information by intersection

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,26 +56,26 @@ collisions.each do |collision|
       borough: collision['borough'],
       lat: collision['latitude'],
       lon: collision['longitude'],
-      persons_injured: collision['number_of_persons_injured'],
-      persons_killed: collision['number_of_persons_killed'],
-      pedestrians_injured: collision['number_of_pedestrians_injured'],
-      pedestrians_killed: collision['number_of_pedestrians_killed'],
-      cyclists_injured: collision['number_of_cyclist_injured'],
-      cyclists_killed: collision['number_of_cyclist_killed'],
-      motorists_injured: collision['number_of_motorist_injured'],
-      motorists_killed: collision['number_of_motorist_killed'],
+      persons_injured: collision['number_of_persons_injured'].to_i,
+      persons_killed: collision['number_of_persons_killed'].to_i,
+      pedestrians_injured: collision['number_of_pedestrians_injured'].to_i,
+      pedestrians_killed: collision['number_of_pedestrians_killed'].to_i,
+      cyclists_injured: collision['number_of_cyclist_injured'].to_i,
+      cyclists_killed: collision['number_of_cyclist_killed'].to_i,
+      motorists_injured: collision['number_of_motorist_injured'].to_i,
+      motorists_killed: collision['number_of_motorist_killed'].to_i,
       collisions: 1
     }
   else
     intersections[location][:collisions] += 1
-    intersections[location][:persons_injured] += collision['number_of_persons_injured']
-    intersections[location][:persons_killed] += collision['number_of_persons_killed']
-    intersections[location][:pedestrians_injured] += collision['number_of_pedestrians_injured']
-    intersections[location][:pedestrians_killed] += collision['number_of_pedestrians_killed']
-    intersections[location][:cyclists_injured] += collision['number_of_cyclist_injured']
-    intersections[location][:cyclists_killed] += collision['number_of_cyclist_killed']
-    intersections[location][:motorists_injured] += collision['number_of_motorist_injured']
-    intersections[location][:motorists_killed] += collision['number_of_motorist_killed']
+    intersections[location][:persons_injured] += collision['number_of_persons_injured'].to_i
+    intersections[location][:persons_killed] += collision['number_of_persons_killed'].to_i
+    intersections[location][:pedestrians_injured] += collision['number_of_pedestrians_injured'].to_i
+    intersections[location][:pedestrians_killed] += collision['number_of_pedestrians_killed'].to_i
+    intersections[location][:cyclists_injured] += collision['number_of_cyclist_injured'].to_i
+    intersections[location][:cyclists_killed] += collision['number_of_cyclist_killed'].to_i
+    intersections[location][:motorists_injured] += collision['number_of_motorist_injured'].to_i
+    intersections[location][:motorists_killed] += collision['number_of_motorist_killed'].to_i
   end
 
   # intersectionArray.push({street_name: collision['on_street_name'], cross_street: collision['off_street_name'], borough: collision['borough']})
@@ -88,8 +88,16 @@ intersections.keys.each do |location|
     borough: intersections[location][:borough],
     lat: intersections[location][:lat],
     lon: intersections[location][:lon],
-    collisions: intersections[location][:collisions]
-    # Maybe include list of unique IDs that populated the data for each intersection
+    collisions: intersections[location][:collisions],
+    persons_injured: intersections[location][:persons_injured],
+    persons_killed: intersections[location][:persons_killed],
+    pedestrians_injured: intersections[location][:pedestrians_injured],
+    pedestrians_killed: intersections[location][:pedestrians_killed],
+    cyclists_injured: intersections[location][:cyclists_injured],
+    cyclists_killed: intersections[location][:cyclists_killed],
+    motorists_injured: intersections[location][:motorists_injured],
+    motorists_killed: intersections[location][:motorists_killed]
+    # Maybe include list of unique IDs that populated the data for each intersection?
   )
 end
 
